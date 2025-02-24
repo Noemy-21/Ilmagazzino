580b75a0e8d88f77cc54b6752273eeee0fdae0da
Riccardo <130995229+RiccardoScimia@users.noreply.github.com>
2025-02-24 14:07:50 +0100

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,32 +1,14 @@
-class gestione_Magazzino:
-    def _init__(self):
-        self.magazzino = []
-    
-    def aggiorna_quantita(self,codice,quantita): #task 2
-        if operazione == "Rifornimento":
-            self.magazzino[codice][quantita] += variazione
-        elif operazione == "Vendita":
-            self.magazzino[codice][quantita] -= variazione
+    def aggiorna_quantita(self, codice, operazione, variazione):  # Task 2
+        if codice in self.magazzino:
+            variazione = int(variazione)
+            if operazione.lower() == "rifornimento":
+                self.magazzino[codice]["quantita"] += variazione
+            elif operazione.lower() == "vendita":
+                if self.magazzino[codice]["quantita"] >= variazione:
+                    self.magazzino[codice]["quantita"] -= variazione
+                else:
+                    print("Errore: Quantità insufficiente per la vendita.")
+            else:
+                print("Operazione non valida.")
         else:
-            print("Operazione non valida")
-
-magazzino = gestione_Magazzino()
-
-while True:
-    print("1. Aggiungi un prodotto al magazzino")
-    print("2. Aggiorna la quantità di un prodotto")
-    print("3. Visualizza l'inventario attuale")
-    print("4. Cerca un prodotto nel magazzino tramite ID")
-    print()
-    print("5. Esci")
-    print("-----------------------------")
-
-    scelta = input("Seleziona un numero: ")
-
- elif scelta == "2":
-        id_aggiornamento = input("Inserisci il codice identificativo del prodotto da aggiornare: ")
-        if id_aggiornamento in self.maagazzino[codice]:
-            operazione = input("Vuoi effettuare un rifornimento o una vendita? ")
-            variazione = input("Inserisci la variazione: ")
-        magazzino.aggiorna_quantita(id_aggiornamento,quantita, operazione, variazione)
-        print("Quantità del prodotto aggiornata")
+            print("Prodotto non trovato nel magazzino.")
