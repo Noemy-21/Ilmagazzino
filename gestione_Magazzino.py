4c16b7d5defec100abe78d42a8cd8ad2044651ab
Noemy-21 <nraimondi@itsmeccanicabruzzo.eu>
2025-02-24 12:05:06 +0100

    Update gestione_Magazzino.py
    magazzino

--- gestione_Magazzino.py
+++ gestione_Magazzino.py
@@ -14,20 +14,3 @@ class gestione_Magazzino:
             })
 
 
-magazzino = gestione_Magazzino()
-
-while True:
-    print("1. Aggiungi un prodotto al magazzino")
-    print("2. Aggiorna la quantità di un prodotto")
-    print("3. Visualizza l'inventario attuale")
-    print("4. Cerca un prodotto nel magazzino tramite ID")
-    print()
-    print("5. Esci")
-    print("-----------------------------")
-
-    scelta = input("Seleziona un numero: ")
-
-    if scelta == "1":
-        id_aggiunta = input("Inserisci il codice identificativo del prodotto da aggiungere al magazzino: ")
-        magazzino.aggiungi_prodotto(id_aggiunta, nome, codice, quantita, prezzo)
-        print("Prodotto aggiunto al magazzino")
