a2ae1e9cb935e5b17c75af2500fa8999d5934bc1
Noemy-21 <nraimondi@itsmeccanicabruzzo.eu>
2025-02-24 14:48:17 +0100

    Update Ilmagazzino.py

--- Ilmagazzino.py
+++ Ilmagazzino.py
@@ -4,8 +4,6 @@ class GestioneMagazzino:
 
     def aggiungi_prodotto(self, nome, codice, quantita, prezzo):  # Task 1
 
-    # errore in Task 1 : se viene inserito un codice già presente nel magazzino, il prodotto viene aggiunto comunque
-
         if codice in self.magazzino:
             print("Il prodotto è già presente nel magazzino.") #!!! il controllo non funziona
         else:
