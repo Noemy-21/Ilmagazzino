bcd9f4f6cd4d7b5d7faead21098e651123621e75
Noemy-21 <nraimondi@itsmeccanicabruzzo.eu>
2025-02-24 13:28:51 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,16 +1,7 @@
-  
-    def stampa_dettagli_ricerca(): #task 4
-        for prodotto in dettagli_ricerca:
-            print(prodotto)
-
-    elif scelta == "4":
-        dettagli_ricerca = []
-
-        id_ricerca = input("Inserisci il codice identificativo del prodotto da cercare: ")
-
-        if id_ricerca in self.magazzino[codice]:
-            if prodotto[codice] == id_ricerca:
-                dettagli_ricerca.append(prodotto)
-            print("Prodotto trovato")
+  def cerca_prodotto(self, codice):  # Task 4
+        if codice in self.magazzino:
+            prodotto = self.magazzino[codice]
+            print(f"Prodotto trovato: Codice: {codice}, Nome: {prodotto['nome']}, Quantità: {prodotto['quantita']}, Prezzo: {prodotto['prezzo']:.2f}€")
         else:
-            print("Prodotto non trovato")
+            print("Prodotto non trovato.")
+   
